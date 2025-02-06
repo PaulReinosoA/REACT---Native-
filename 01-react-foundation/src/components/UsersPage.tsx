eeec1f8cdc688215a05a8dbc4835997b10fd952a
Washington Reinoso <Washington.Reinoso@Portales.Local>
2025-02-06 14:30:31 -0500

    22. Custom Hook Users

--- 01-react-foundation/src/components/UsersPage.tsx
+++ 01-react-foundation/src/components/UsersPage.tsx
@@ -1,15 +1,16 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import type { ReqResUserListResponse, User } from '../interfaces';
 
-const loadUsers = async (): Promise<User[]> => {
+const loadUsers = async (page: number = 1): Promise<User[]> => {
   // fetch('https://reqres.in/api/users?page=2')
   //   .then((resp) => resp.json())
   //   .then((data) => console.log(data));
 
   try {
     const { data } = await axios.get<ReqResUserListResponse>(
-      'https://reqres.in/api/users?page=2'
+      'https://reqres.in/api/users',
+      { params: { page: page } }
     );
     return data.data;
   } catch (error) {
@@ -20,10 +21,28 @@ const loadUsers = async (): Promise<User[]> => {
 
 export const UsersPage = () => {
   const [users, addUsers] = useState<User[]>([]);
+  const currentPageRef = useRef(1); //hook para cada vez que se llame tener el valor
 
   useEffect(() => {
-    loadUsers().then((users) => addUsers(users));
+    loadUsers(currentPageRef.current).then((users) => addUsers(users));
   }, []);
+
+  const nextPage = async () => {
+    currentPageRef.current++;
+    const users = await loadUsers(currentPageRef.current);
+    if (users.length > 0) {
+      addUsers(users);
+    } else {
+      currentPageRef.current--;
+    }
+  };
+
+  const prevPage = async () => {
+    if (currentPageRef.current < 1) return;
+    currentPageRef.current--;
+    const users = await loadUsers(currentPageRef.current);
+    addUsers(users);
+  };
 
   return (
     <>
@@ -38,14 +57,32 @@ export const UsersPage = () => {
         </thead>
         <tbody>
           {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.avatar}</td>
-              <td>{user.first_name}</td>
-              <td>{user.email}</td>
-            </tr>
+            <UserRow key={user.id} user={user} />
           ))}
         </tbody>
       </table>
+      <div>
+        <button onClick={() => prevPage()}>Prev</button>
+        <button onClick={() => nextPage()}>Next</button>
+      </div>
     </>
+  );
+};
+
+interface Props {
+  user: User;
+}
+
+export const UserRow = ({ user }: Props) => {
+  const { avatar, first_name, email } = user;
+
+  return (
+    <tr>
+      <td>
+        <img style={{ width: '45px', height: 'auto' }} src={avatar} alt="" />
+      </td>
+      <td>{first_name}</td>
+      <td>{email}</td>
+    </tr>
   );
 };
