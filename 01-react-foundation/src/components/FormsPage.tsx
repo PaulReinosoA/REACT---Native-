4e9d60d1780aeac7a69ef3eaf1ffe41b3adb5683
PaulReinosoA <164814520+PaulReinosoA@users.noreply.github.com>
2025-02-10 23:11:01 -0500

    24. react-hook-form

--- 01-react-foundation/src/components/FormsPage.tsx
+++ 01-react-foundation/src/components/FormsPage.tsx
@@ -1,14 +1,39 @@
+import { useForm } from 'react-hook-form';
+
+type FormInputs = {
+  email: string;
+  password: string;
+};
+
 export const FormsPage = () => {
+  const { register, handleSubmit } = useForm<FormInputs>({
+    defaultValues: {
+      email: 'paulreinoso@gmail.com',
+      password: 'abc123',
+    },
+  });
+
+  const onSubmit = (myFor: FormInputs) => {
+    console.log({ myFor });
+  };
+
   return (
     <>
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h3>FormsPage</h3>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="password" />
-          <button>Ingresar</button>
+          <input type="text" placeholder="Email" {...register('email')} />
+          <input
+            type="password"
+            placeholder="password"
+            {...register('password')}
+          />
+          <button type="submit">Ingresar</button>
         </div>
       </form>
     </>
   );
 };
+
+//* https://react-hook-form.com/get-started
+// con esto no tengo reRenders
