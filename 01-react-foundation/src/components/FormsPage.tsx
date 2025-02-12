15958aaa4d47184c0cbaaf6a152b8c693b88489f
Washington Reinoso <Washington.Reinoso@Portales.Local>
2025-02-12 17:34:13 -0500

    30. instalacion react native

--- 01-react-foundation/src/components/FormsPage.tsx
+++ 01-react-foundation/src/components/FormsPage.tsx
@@ -6,7 +6,7 @@ type FormInputs = {
 };
 
 export const FormsPage = () => {
-  const { register, handleSubmit } = useForm<FormInputs>({
+  const { register, handleSubmit, formState, watch } = useForm<FormInputs>({
     defaultValues: {
       email: 'paulreinoso@gmail.com',
       password: 'abc123',
@@ -17,12 +17,18 @@ export const FormsPage = () => {
     console.log({ myFor });
   };
 
+  console.log(watch('email'));
+
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <h3>FormsPage</h3>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <input type="text" placeholder="Email" {...register('email')} />
+          <input
+            type="text"
+            placeholder="Email"
+            {...register('email', { required: true })}
+          />
           <input
             type="password"
             placeholder="password"
@@ -31,6 +37,7 @@ export const FormsPage = () => {
           <button type="submit">Ingresar</button>
         </div>
       </form>
+      <pre>{JSON.stringify(formState, null, 2)}</pre>
     </>
   );
 };
